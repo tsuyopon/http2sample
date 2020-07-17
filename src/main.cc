7d9f873a69f6b7083c985ebc353eb00f20a780c7
Tsuyoshi Ooishi <tsuyoshiooishi@yahoo.co.jp>
2020-07-17 23:33:12 +0900

    create RequestUtil.h and RequestUtil.cc

--- src/main.cc
+++ src/main.cc
@@ -38,6 +38,7 @@
 #include "DebugUtils.h"
 #include "ErrorCodes.h"
 #include "HuffmanCode.h"
+#include "RequestUtil.h"
 
 // ALPN識別子. h2
 static const unsigned char protos[] = { 0x02, 0x68, 0x32 };
@@ -46,58 +47,6 @@ static int protos_len = 3;
 
 int get_error();
 void close_socket(SOCKET socket, SSL_CTX *_ctx, SSL *_ssl);
-
-// URLをパースして、schme, host, pathを返す
-int8_t parseUrl(std::string url, std::string &scheme, std::string &host, std::string &path){
-
-	std::string https_prefix = "https://";
-	if (url.size() > https_prefix.size() && std::equal(std::begin(https_prefix), std::end(https_prefix), std::begin(url))) {  // 先頭prefixに一致することの確認
-		scheme = "https";  // https以外返さない
-
-		// 先頭から8byte(https://)は除去
-		url.erase(0, https_prefix.size());
-
-		// FIXME: urlはポート番号(:443等)は考慮していない
-		// スラッシュ(/)で前後を分解して、前はドメイン、後ろはURLとする。スラッシュが存在しない場合はpathに"/"を指定する。
-		size_t slash_pos = url.find("/");
-		if( slash_pos == std::string::npos) {
-			// not found slash
-			host = url;  
-			path = "/";
-		} else {
-			// found slash
-			host =  url.substr(0, url.find("/"));
-			path =  url.substr(url.find("/"));
-		}
-	} else {
-		printf("URL Parse Error. Maybe \"https://\" is not include.\n");
-		return -1;
-	}
-	return 0;
-}
-
-char asciitolower(char in) {
-    if (in <= 'Z' && in >= 'A')
-        return in - ('Z' - 'z');
-    return in;
-}
-
-// ヘッダで「Name: Value」形式を分解して、header_name、header_valueに値を格納する
-int8_t parseHeader(std::string header, std::string &header_name, std::string &header_value){
-	size_t colon_pos = header.find(":");
-	if( colon_pos == std::string::npos) {
-		printf("Header Parser Error. \":\" separate field is not found\n");
-		return -1;
-	}
-	header_name =  header.substr(0, header.find(":"));
-	// header field names MUST be converted to lowercase prior to their encoding in HTTP/2.  A request or response containing uppercase header field names MUST be treated as malformed (sec8.1.2)
-	std::transform(header_name.begin(), header_name.end(), header_name.begin(), asciitolower);
-
-	header_value=  header.substr(header.find(":"));
-	return 0;
-}
-
-
 
 int main(int argc, char **argv)
 {
@@ -130,7 +79,7 @@ int main(int argc, char **argv)
 			case 'u':  // url option
 					printf("URL(u) recieved. %s\n", optarg);
 					url = optarg;
-					parseUrl(url, scheme, host, path);
+					RequestUtil::parseUrl(url, scheme, host, path);
 					headers[":scheme"] = scheme.c_str();
 					headers[":authority"] = host.c_str();
 					headers[":path"] = path.c_str();
@@ -141,7 +90,7 @@ int main(int argc, char **argv)
 					url = optarg;
 					std::string header_value;
 					std::string header_name;
-					parseHeader(optarg, header_value, header_name);
+					RequestUtil::parseHeader(optarg, header_value, header_name);
 					headers[header_value] = header_name;
 			}
 				break;
